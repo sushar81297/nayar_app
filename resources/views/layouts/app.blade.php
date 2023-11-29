2d2d14099438bacf361730385e3bad221a13221d
unknown <heinsett01@gmail.com>
2023-11-29 11:38:40 +0630

    add high chart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,9 @@
   <!-- Nepcha Analytics (nepcha.com) -->
   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
+  {{-- High chart cdn --}}
+  <script src="https://code.highcharts.com/highcharts.js"></script>
+
 </head>
 
 <body class="g-sidenav-show  bg-gray-200">
--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -1,7 +1,76 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <section class="content">
+    <div style="display: flex">
+        <div class="col-lg-6"><div id="chart-container" style="height: 400px; padding-right:2px"></div>
+    </div>
+        <div class="col-lg-6"><div id="bar-chart-container" style="height: 400px; padding-left:2px"></div>
+    </div>
+
+    </div>
+<br>
+    <script>
+        // Sample data for the chart
+        var data = [5, 10, 15, 20, 25];
+        
+        // Create the chart
+        Highcharts.chart('chart-container', {
+          chart: {
+            type: 'line' // Specify the chart type
+          },
+          title: {
+            text: 'Simple Line Chart Demo'
+          },
+          xAxis: {
+            categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'] // X-axis labels
+          },
+          yAxis: {
+            title: {
+              text: 'Values' // Y-axis label
+            }
+          },
+          series: [{
+            name: 'Data Series',
+            data: data // Actual data points
+          }]
+        });
+        </script>
+        <script>
+            // Sample data for the bar chart
+            var data = [5, 10, 15, 20, 25];
+          
+            // Create the bar chart
+            Highcharts.chart('bar-chart-container', {
+              chart: {
+                type: 'bar' // Specify the chart type as a bar chart
+              },
+              title: {
+                text: 'Highcharts Bar Chart Demo' // Chart title
+              },
+              xAxis: {
+                categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'], // X-axis labels
+                title: {
+                  text: 'Categories' // X-axis title
+                }
+              },
+              yAxis: {
+                title: {
+                  text: 'Values' // Y-axis title
+                }
+              },
+              series: [{
+                name: 'Data Series',
+                data: data // Actual data points for the bar chart
+              }]
+            });
+          </script>
+
+    
+    
+
     @forelse ($posts as $post)
     <div class="col-lg-12 col-md-6 mb-4">
         <div class="card">
